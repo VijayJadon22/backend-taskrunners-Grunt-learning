c5c0a5feea18004f4c5f2b9130faa0cb75026625
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2024-12-07 20:19:05 +0530

    implement minify css with grunt-contrib-cssmin

--- gruntFile.js
+++ gruntFile.js
@@ -2,22 +2,37 @@ module.exports = function (grunt) {
 
     // Project configuration
     grunt.initConfig({
-        // Read the package metadata from package.json
         pkg: grunt.file.readJSON('package.json'),
 
-        // Uglify task configuration
+        // Uglify task configuration for JavaScript files
         uglify: {
             target: {
                 files: {
-                    "destination/js/output.min.js": ["src/js/index.js"] // Specify source file and output destination
+                    'destination/js/output.min.js': ['src/js/index.js'] // Source file and output destination for JS
                 }
+            }
+        },
+
+        // CSSMin task configuration for CSS files using the earlier mentioned syntax
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css', // Current working directory
+                    src: ['*.css', '!*.min.css'], // Source files pattern
+                    dest: 'destination/css', // Destination directory
+                    ext: '.min.css' // Extension for minified files
+                }]
             }
         }
     });
 
-    // Load the plugin that provides the "uglify" task
+    // Load the plugins that provide the "uglify" and "cssmin" tasks
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    // Register default task(s)
-    grunt.registerTask('default', ['uglify']);
+    // Default task(s)
+    grunt.registerTask('default', ['uglify', 'cssmin']);
 };
+
+
